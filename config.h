d403d1dd76aff0e2d1085470ec7257144ff96f78
Nick Clark <nicholas.clark@gmail.com>
2018-04-05 07:00:22 -0600

    Added tiny config-file parser,
    This commit adds a small 200-ish line config file parser. The parser
    is pretty low-performance, but it's simple and easy to use.

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define _POSIX_C_SOURCE 200809L
+
+#endif
+
+
---
+++ scan.h
@@ -0,0 +1,7 @@
+#ifndef _SCAN_H_
+#define _SCAN_H_
+
+int find_key(const char *filename, const char *section, const char *key,
+             char **value);
+
+#endif
