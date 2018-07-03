5d7f8941aa61daa9fe5b9e6c96574a60d77b302a
Nick Clark <nclark@uaudio.com>
2018-07-03 07:26:54 -0700

    Remove extra files from legacy project

--- mkpath.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MKPATH_H_
-#define _MKPATH_H_
-#include <sys/types.h>
-
-int mkpath(const char *path, mode_t mode);
-extern const char *progname;
-
-#endif
--- scan.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SCAN_H_
-#define _SCAN_H_
-
-int find_key(const char *filename, const char *section, const char *key,
-             char **value);
-
-#endif
