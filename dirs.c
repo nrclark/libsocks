e86187ff4e5a715307b82e6fa6f074cca670eed0
Nick Clark <nicholas.clark@gmail.com>
2018-09-25 16:06:36 -0600

    dirs.c: rearrange includes

--- dirs.c
+++ dirs.c
@@ -1,15 +1,15 @@
 #define _POSIX_C_SOURCE 200809L
 #define _XOPEN_SOURCE 700
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <dirent.h>
-#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "debug.h"
 
